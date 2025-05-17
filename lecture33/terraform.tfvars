29ad3f7c423b96271245a9b53954147ed364d649
masato-N918 <b152122y@gmail.com>
2025-05-17 16:14:09 +0900

    tfvarsファイルの作成、variablesファイルの修正

---
+++ lecture33/terraform.tfvars
@@ -0,0 +1,4 @@
+db_name = "awsstudy"
+db_password = "rootroot"
+key_name = "udemysample"
+username  = "root"
--- lecture33/variables.tf
+++ lecture33/variables.tf
@@ -1,7 +1,23 @@
+variable "db_name" {
+  type        = string
+  description = "name of the database instance"
+}
+
+variable "db_password" {
+  type        = string
+  description = "password for the master database user"
+  sensitive = true
+}
+
 variable "instance_type" {
   type        = string
   description = "ec2 instance type"
   default     = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "name of the ec2 instance key"
 }
 
 variable "pri_subnets" {
@@ -20,6 +36,11 @@ variable "pub_subnets" {
     "1a" = "10.0.1.0/24"
     "1c" = "10.0.2.0/24"
   }
+}
+
+variable "username" {
+  type        = string
+  description = "username for the master database user"
 }
 
 variable "vpc_cidr" {
