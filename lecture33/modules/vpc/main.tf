ec2e8955613e26797cbcfb011866b959f53423a0
masato-N918 <b152122y@gmail.com>
2025-07-13 18:49:46 +0900

    修正：ポート設定

--- lecture33/modules/vpc/main.tf
+++ lecture33/modules/vpc/main.tf
@@ -135,8 +135,6 @@ resource "aws_vpc_security_group_egress_rule" "allow_all" {
   security_group_id = aws_security_group.app_server.id
 
   ip_protocol = "-1"
-  from_port   = 0
-  to_port     = 0
   cidr_ipv4   = "0.0.0.0/0"
 }
 
